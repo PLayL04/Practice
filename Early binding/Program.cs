1f98aea2a059df27cf13d5e0acd49c7014fdfa5f
PLayL04 <playl04@yandex.ru>
2022-11-28 12:50:15 +0700

    Добавил 7ю лабу

---
+++ Early binding/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Early_binding
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
